3e55ccc7f52b8e1562d9ee903faacf22c3f9ac90
csarj <csarjdeveloper@gmail.com>
2025-02-18 19:07:02 -0500

    Creación de los controles para las rutas admin

--- routes/products.js
+++ routes/products.js
@@ -1,81 +1,12 @@
 const express = require('express');
 const router = express.Router();
-var path = require('path');
-var fs = require('fs');
+const productController = require('../controllers/products-controller')
 
 
-router.get('/', async (req, res) => {
+router.get('/', productController.index)
 
+router.get('/new-item-form', productController.create)
 
-    let leerJSON = async (src) => {
-        try {
-            const data = await fs.readFileSync(src, 'utf-8');
-            return JSON.parse(data);
-        } catch (error) {
-            console.error('Error al leer el archivo JSON: ', error);
-        }
-    }
-
-    try {
-
-        let src = path.join(__dirname, '../DB/phones.json');
-        console.log(src);
-
-        const data = await leerJSON(src);
-
-        let jsonString = JSON.stringify(data)
-        let phones = JSON.parse(jsonString)
-
-        res.render('./products/app-crud', { title: 'CRUD', phones })
-
-    } catch (error) {
-        res.status(500).json({ error: 'Error al obtener telefonos' });
-    }
-})
-
-router.get('/new-item-form', (req, res) => {
-    try {
-        res.render('./products/newItem-form', { title: 'Create Item' })
-    } catch (error) {
-        res.status(500).send('Error al cargar página');
-    }
-})
-
-router.get('/mod-item-form/item-:id', (req, res) => {
-    const id = req.params.id
-    const src = path.join(__dirname, '../DB/phones.json');
-
-    // Leer el archivo listado.json
-    fs.readFile(src, 'utf8', (err, data) => {
-        if (err) {
-            console.error("Error al leer el archivo JSON:", err);
-            return res.status(500).send("Error interno del servidor");
-        }
-
-        try {
-            const phones = JSON.parse(data);
-            // Buscar el celular que coincida con el ID
-            const item = phones.find(phone =>
-                phone.id === id
-            );
-
-            if (!item) {
-                return res.status(404).render("error", {
-                    error: {
-                        message: 'Error al cargar pagina',
-                        status: '404 Not Found',
-                        stack: 'No se encontro el producto',
-                    }
-                });
-            }
-            // Renderizar la vista y pasar el objeto del celular
-            res.render('./products/modItem-form', { title: 'Modify item', item });
-
-        } catch (parseError) {
-            console.error("Error al analizar JSON:", parseError);
-            return res.status(500).send("Error interno del servidor");
-        }
-    })
-})
+router.get('/mod-item-form/item-:id', productController.edit)
 
 module.exports = router
